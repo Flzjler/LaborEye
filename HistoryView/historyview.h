87b93aaac8b411a1cfa0d42aab4919c02d3eed39
Flzjler <42337024+Flzjler@users.noreply.github.com>
2019-11-18 18:38:19 +0800

    修复部分BUG

--- HistoryView/historyview.h
+++ HistoryView/historyview.h
@@ -15,28 +15,32 @@ class HistoryView : public QWidget
 
 public:
     explicit HistoryView(QWidget *parent = nullptr);
+
     ~HistoryView();
 
 private slots:
-    void on_btnPrepage_clicked();
+    void on_btnPrepage_clicked();       //点击上一页
 
-    void on_btnNxtpage_clicked();
+    void on_btnNxtpage_clicked();       //点击下一页
 
-    void on_btnConfirm_clicked();
+    void on_btnConfirm_clicked();       //点击确定
 
-    void on_btnJmppage_clicked();
+    void on_btnJmppage_clicked();       //点击跳转
 
 private:
     Ui::HistoryView *ui;
 
-    static int pageSize;    //每页显示的记录数
-    static int tolPages;    //总页数
-    static int nowPage;     //当前页码
-    const int colNum = 3;   //表格列数
+    static int pageSize;                //每页显示的记录数
+    static int tolPages;                //总页数
+    static int nowPage;                 //当前页码
 
-    void initUI();
-    void setTblItem();
-    QList<RecordInfo> getRecordInfo();
+    const int colNum = 3;               //表格列数
+
+    void initUI();                      //初始化UI
+
+    void setTblItem();                  //设置表格内容
+
+    QList<RecordInfo> getRecordInfo();  //获取历史记录
 
 };
 
