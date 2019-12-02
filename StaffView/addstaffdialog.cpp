f00c93cc95d452a5dce2c7692ef8c1b03f42e259
Flzjler <42337024+Flzjler@users.noreply.github.com>
2019-12-02 12:08:34 +0800

    更新部分功能

--- StaffView/addstaffdialog.cpp
+++ StaffView/addstaffdialog.cpp
@@ -13,7 +13,38 @@ AddStaffDialog::~AddStaffDialog()
     delete ui;
 }
 
+void AddStaffDialog::initUI()
+{
+    avatarPicPath = "";
+    ui->ledtName->clear();
+    ui->ledtAvatar->clear();
+    ui->ledtIdCard->clear();
+    ui->ledtRole->clear();
+    ui->ledtAddress->clear();
+    ui->ledtContact->clear();
+}
+
 void AddStaffDialog::showAddStaffDialog()
 {
+    initUI();
     this->show();
+}
+
+void AddStaffDialog::on_btnConcel_clicked()
+{
+    initUI();
+    this->close();
+}
+
+void AddStaffDialog::on_btnConfrim_clicked()
+{
+
+}
+
+void AddStaffDialog::on_btnAvatar_clicked()
+{
+    avatarPicPath = QFileDialog::getOpenFileName(this,
+                                                 "请选择证件照",
+                                                 "",
+                                                 "Image Files(*.jpg)");
 }
--- StaffView/addstaffdialog.h
+++ StaffView/addstaffdialog.h
@@ -2,6 +2,8 @@
 #define ADDSTAFFDIALOG_H
 
 #include <QDialog>
+#include <QDebug>
+#include <QFileDialog>
 
 namespace Ui {
 class AddStaffDialog;
@@ -18,8 +20,19 @@ public:
 public slots:
     void showAddStaffDialog();
 
+private slots:
+    void on_btnConcel_clicked();
+
+    void on_btnConfrim_clicked();
+
+    void on_btnAvatar_clicked();
+
 private:
     Ui::AddStaffDialog *ui;
+    QString avatarPicPath;
+
+    void initUI();
+
 };
 
 #endif // ADDSTAFFDIALOG_H
